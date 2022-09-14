ed70d6241f2f47bd80873225906b89d758e34864
Johnathan <jonathanumana86@gmail.com>
2022-09-14 17:03:24 -0400

    Added counter & show id

--- accounts/src/app/services/counter.service.ts
+++ accounts/src/app/services/counter.service.ts
@@ -4,9 +4,22 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CounterService {
+  activetoInActive = 0
+  inactiveToActive = 0
+
+  incrementInactivetoActive(){
+    const moreInactive = this.activetoInActive++
+    console.log('Inactive to active ' + ' ' + moreInactive);
+  }
+
+  incrementActivetoInactive(){
+    const moreActive = this.inactiveToActive++
+    console.log('Active to Inactive' + ' ' + moreActive);
+
+  }
+
   showNums(id: number){
     console.log(id);
-
   }
 
   constructor() { }
--- accounts/src/app/services/users.service.ts
+++ accounts/src/app/services/users.service.ts
@@ -13,13 +13,13 @@ export class UsersService {
   onSetToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
-    this.numSer.showNums(id)
+    this.numSer.incrementActivetoInactive()
   }
 
   onSetToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
-    this.numSer.showNums(id)
+    this.numSer.incrementInactivetoActive()
   }
 
 }
